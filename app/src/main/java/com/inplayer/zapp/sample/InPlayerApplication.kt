6b197df2c637cad97d1078bd1ae60bb5c77b0746
sarevd <sarevd@gmail.com>
2020-01-10 01:50:49 +0100

    added inplayer sdk initialized sdk

---
+++ app/src/main/java/com/inplayer/zapp/sample/InPlayerApplication.kt
@@ -0,0 +1,21 @@
+package com.inplayer.zapp.sample
+
+import android.app.Application
+import com.sdk.inplayer.configuration.InPlayer
+
+
+class InPlayerApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        InPlayer.initialize(
+            InPlayer.Configuration.Builder(
+                this,
+                "cd3f233c-4045-47ef-bece-f7221b0bd07c"
+            ) //Optional
+                .withReferrer("zapp-ios-test") //Optional -Default is set to Production
+                .withEnvironment(InPlayer.EnvironmentType.STAGING)
+                .build()
+        )
+    }
+}
